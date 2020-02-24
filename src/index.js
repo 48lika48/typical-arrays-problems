83a662ae1ee90cd2e56b0056c222100187282436
48lika48 <48lika48@mail.ru>
2020-02-24 16:43:10 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array == 0 || array == undefined){
+        return 0;
+    }
+    let newArr = array.sort(function(a,b){ 
+        return a - b;
+      });
+    return newArr[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array == 0 || array == undefined){
+        return 0;
+    }
+    let newArr = array.sort(function(a,b){ 
+        return b - a;
+      });
+    return newArr[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array == 0 || array == undefined){
+        return 0;
+    }
+    let sumNumber = array.reduce(function(previousValue, currentValue) {
+        return previousValue + currentValue;
+    });
+    return sumNumber / array.length;
 }
